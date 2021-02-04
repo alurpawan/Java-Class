643cb5cfc77b5b20e859a07297fb0585db576291
Pawan Alur <alur.pawan.pa@gmail.com>
2021-02-04 12:34:22 -0800

    Adding Program 1

---
+++ Program 1/HelloWorld.java
@@ -0,0 +1,45 @@
+/**
+ * This is First Program demonstrating different data types.
+ */
+class HelloWorld {
+    public static void main(String[] args) {
+        int sampleInt = 62964;
+        System.out.println("The maximum integer is: " + Integer.MAX_VALUE);
+        System.out.println("A random integer is: " + sampleInt);
+        System.out.println("");
+        
+        short sampleShort = 3210;
+        System.out.println("The maximum short is: " + Short.MAX_VALUE);
+        System.out.println("A random short is: " + sampleShort);
+        System.out.println("");
+
+        long sampleLong = 2627293267L;
+        System.out.println("The maximum long is: " + Long.MAX_VALUE);
+        System.out.println("A random long is: " + sampleLong);
+        System.out.println("");
+
+        byte sampleByte = 122;
+        System.out.println("The maximum byte is: " + Byte.MAX_VALUE);
+        System.out.println("A random byte is: " + sampleByte);
+        System.out.println("");
+
+        float sampleFloat = 122.456f;
+        System.out.println("The maximum float is: " + Float.MAX_VALUE);
+        System.out.println("A random float is: " + sampleFloat);
+        System.out.println("");
+
+        double sampleDouble = 1462.2346;
+        System.out.println("The maximum double is: " + Double.MAX_VALUE);
+        System.out.println("A random double is: " + sampleDouble);
+        System.out.println("");
+
+        char sampleChar = 'C';
+        System.out.println("A random char is: " + sampleChar);
+        System.out.println("");
+        
+        boolean trueBool = true;
+        boolean falseBool = false;
+        System.out.println("A true boolean is: " + trueBool);
+        System.out.println("A false boolean is: " + falseBool);
+    }
+}
